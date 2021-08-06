d9336349e1da4caf6ce7034a08ef1a6d6d98e553
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-06 21:02:53 +0100

    Add installation const

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+.prompt([
+  {
+    type: "input",
+    name: "Title",
+    message: "What is the title of the project?"
+  },
+  {
+    type: "input",
+    name: "Project",
+    message:  "Give a detailed overview and description of the project"
+  },
+  {
+    type: "input",
+    name: "Acceptance Criteria",
+    message: "Guidence about funtionality",
+  },
+  {
+    type: "input",
+    name: "User Story",
+    message: "Explain the context of the Functionality",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "Explain the context of the Functionality",
+  },
+
+
+  }  
+
+])
+      
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
