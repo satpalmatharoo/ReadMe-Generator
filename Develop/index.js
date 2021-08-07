8372657b7c81b2917e4456244713514a4a452ddd
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-07 20:35:15 +0100

    function for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = inquirer
@@ -27,22 +29,33 @@ const questions = inquirer
   {
     type: "input",
     name: "Installation",
-    message: "Explain the context of the Functionality",
+    message: "Are there any installations are necessary to complete Project?",
   },
 
-
-  }  
-
 ])
-      
-
-
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("fileName","data"), function (err){
+        if (err) throw err;
+        console.log (data)
+    }
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(generateReadme) {
+  this.generateReadme=generateReadme;
+}
+generateReadme.prototype.print=function (){
+  console.log(this.generateReadme)
+}
 
 // Function call to initialize app
 init();
+
+
+var fs = require('fs');
+
+//create a file named mynewfile3.txt:
+
