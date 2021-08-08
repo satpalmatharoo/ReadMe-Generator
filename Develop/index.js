dd2378b269100859964ed7061667b22391e89703
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-08 13:36:55 +0100

    Amend questions to user

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
+const utils = require ("utils");
 
 
 // TODO: Create an array of questions for user input
@@ -13,22 +14,42 @@ const questions = inquirer
   },
   {
     type: "input",
-    name: "Project",
+    name: "Description",
     message:  "Give a detailed overview and description of the project"
   },
   {
     type: "input",
-    name: "Acceptance Criteria",
+    name: "Table of Contents",
     message: "Guidence about funtionality",
   },
   {
     type: "input",
-    name: "User Story",
-    message: "Explain the context of the Functionality",
+    name: "Installation",
+    message: "Provide any installation instructions",
   },
   {
     type: "input",
-    name: "Installation",
+    name: "Usage",
+    message: "Please explain usage",
+  },
+  {
+    type: "input",
+    name: "License",
+    message: "Add your license or badge link if needed",
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "Would you like to add contributors to your project?",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "Do you have any project tests? If so please provide",
+  },
+  {
+    type: "input",
+    name: "Questions",
     message: "Are there any installations are necessary to complete Project?",
   },
 
@@ -36,12 +57,25 @@ const questions = inquirer
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("fileName","data"), function (err){
-        if (err) throw err;
-        console.log (data)
-    }
-    
+  fs.writeFileAsync = utils.promisfy (fs.writeFile);
+
+  const promptUser = () => {
+    return inquirer.prompt ([
+      )
+  }
+  
 }
+
+
+
+
+// // function writeToFile(fileName, data) {
+//     fs.writeFile("fileName","data"), function (err){
+//         if (err) throw err;
+//         console.log (data)
+//     }
+    
+// }
 
 // TODO: Create a function to initialize app
 function init(generateReadme) {
