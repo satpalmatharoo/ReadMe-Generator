87b94eed769e7b98fe4b4bdf9c27bd08e3b1108f
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-21 18:27:38 +0100

    correct md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,6 +10,7 @@ renderLicenseBadge ()
 function renderLicenseLink(license) {}
 renderLicenseLink ()
 
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
