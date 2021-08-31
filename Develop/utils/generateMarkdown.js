6a25faaa2219182ea408bd48538cb33ed790872f
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-31 21:56:28 +0100

    correct license bug

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,34 +1,31 @@
 // Function that returns a license badge based on which license is passed in
-
 function renderLicenseBadge(license) {
-  return (license)
- "[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)";
- "[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)";
- "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0);
-}
+  if (license === 'N/A') {
+    return '';
+  }
 
-renderLicenseBadge ()
+  return `![GitHub License]((https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
 
 // Function that returns the license link
-
 function renderLicenseLink(license) {
-  return (license)
+  if (license === 'N/A') {
+    return '';
+  }
+
+  return '[License](#license)'
 }
-renderLicenseLink ()
 
-
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   return (license)
 }
-renderLicenseSection ()
+
 
 // unction to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return data
 }
 
 module.exports = generateMarkdown;
